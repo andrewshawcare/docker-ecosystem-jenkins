6f63a62ce69175d3a5901652444a544350e47820
Andrew Shaw Care <andrew.shawcare@sermo.com>
2017-09-10 20:51:08 -0400

    Ensure the Jenkins instance is saved after enabling security System.getEnv -> System.getenv

--- master/init.groovy.d/1-enable-security.groovy
+++ master/init.groovy.d/1-enable-security.groovy
@@ -4,8 +4,9 @@ import hudson.security.*
 final boolean allowsSignup = false
 final HudsonPrivateSecurityRealm securityRealm = new HudsonPrivateSecurityRealm(allowsSignup)
 
-final String userName = System.getEnv('JENKINS_ADMINISTRATOR_USERNAME')
-final String password = System.getEnv('JENKINS_ADMINISTRATOR_PASSWORD')
+final String userName = System.getenv('JENKINS_ADMINISTRATOR_USERNAME')
+final String password = System.getenv('JENKINS_ADMINISTRATOR_PASSWORD')
 securityRealm.createAccount(userName, password)
 
-Jenkins.instance.setSecurityRealm(securityRealm)
+Jenkins.instance.setSecurityRealm((SecurityRealm) securityRealm)
+Jenkins.instance.save()
